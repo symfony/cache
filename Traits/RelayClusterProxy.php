2d7fce2c71a95ac7748b2af4bb1969466ec9b369
Christian Flothmann <christian.flothmann@open.de>
2025-10-06 17:10:08 +0200

    fix compatibility with RelayCluster 0.12

--- Traits/RelayClusterProxy.php
+++ Traits/RelayClusterProxy.php
@@ -223,6 +223,11 @@ class RelayClusterProxy extends \Relay\Cluster implements ResetInterface, LazyOb
         return $this->initializeLazyObject()->del(...\func_get_args());
     }
 
+    public function delifeq($key, $value): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->delifeq(...\func_get_args());
+    }
+
     public function discard(): bool
     {
         return $this->initializeLazyObject()->discard(...\func_get_args());
@@ -453,6 +458,16 @@ class RelayClusterProxy extends \Relay\Cluster implements ResetInterface, LazyOb
         return $this->initializeLazyObject()->hgetall(...\func_get_args());
     }
 
+    public function hgetdel($key, $fields): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->hgetdel(...\func_get_args());
+    }
+
+    public function hgetex($hash, $fields, $expiry = null): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->hgetex(...\func_get_args());
+    }
+
     public function hincrby($key, $member, $value): \Relay\Cluster|false|int
     {
         return $this->initializeLazyObject()->hincrby(...\func_get_args());
@@ -521,6 +536,11 @@ class RelayClusterProxy extends \Relay\Cluster implements ResetInterface, LazyOb
     public function hset($key, ...$keys_and_vals): \Relay\Cluster|false|int
     {
         return $this->initializeLazyObject()->hset(...\func_get_args());
+    }
+
+    public function hsetex($key, $fields, $expiry = null): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->hsetex(...\func_get_args());
     }
 
     public function hsetnx($key, $member, $value): \Relay\Cluster|bool
@@ -1008,12 +1028,77 @@ class RelayClusterProxy extends \Relay\Cluster implements ResetInterface, LazyOb
         return $this->initializeLazyObject()->unwatch(...\func_get_args());
     }
 
+    public function vadd($key, $values, $element, $options = null): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->vadd(...\func_get_args());
+    }
+
+    public function vcard($key): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->vcard(...\func_get_args());
+    }
+
+    public function vdim($key): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->vdim(...\func_get_args());
+    }
+
+    public function vemb($key, $element, $raw = false): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->vemb(...\func_get_args());
+    }
+
+    public function vgetattr($key, $element, $raw = false): \Relay\Cluster|array|false|string
+    {
+        return $this->initializeLazyObject()->vgetattr(...\func_get_args());
+    }
+
+    public function vinfo($key): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->vinfo(...\func_get_args());
+    }
+
+    public function vismember($key, $element): \Relay\Cluster|bool
+    {
+        return $this->initializeLazyObject()->vismember(...\func_get_args());
+    }
+
+    public function vlinks($key, $element, $withscores): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->vlinks(...\func_get_args());
+    }
+
+    public function vrandmember($key, $count = 0): \Relay\Cluster|array|false|string
+    {
+        return $this->initializeLazyObject()->vrandmember(...\func_get_args());
+    }
+
+    public function vrange($key, $end, $start, $count = -1): \Relay\Cluster|array|bool
+    {
+        return $this->initializeLazyObject()->vrange(...\func_get_args());
+    }
+
+    public function vrem($key, $element): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->vrem(...\func_get_args());
+    }
+
+    public function vsetattr($key, $element, $attributes): \Relay\Cluster|false|int
+    {
+        return $this->initializeLazyObject()->vsetattr(...\func_get_args());
+    }
+
+    public function vsim($key, $member, $options = null): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->vsim(...\func_get_args());
+    }
+
     public function watch($key, ...$other_keys): \Relay\Cluster|bool
     {
         return $this->initializeLazyObject()->watch(...\func_get_args());
     }
 
-    public function waitaof(array|string $key_or_address, int $numlocal, int $numremote, int $timeout): \Relay\Cluster|array|false
+    public function waitaof($key_or_address, $numlocal, $numremote, $timeout): \Relay\Cluster|array|false
     {
         return $this->initializeLazyObject()->waitaof(...\func_get_args());
     }
@@ -1021,6 +1106,11 @@ class RelayClusterProxy extends \Relay\Cluster implements ResetInterface, LazyOb
     public function xack($key, $group, $ids): \Relay\Cluster|false|int
     {
         return $this->initializeLazyObject()->xack(...\func_get_args());
+    }
+
+    public function xackdel($key, $group, $ids, $mode = null): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->xackdel(...\func_get_args());
     }
 
     public function xadd($key, $id, $values, $maxlen = 0, $approx = false, $nomkstream = false): \Relay\Cluster|false|string
@@ -1041,6 +1131,11 @@ class RelayClusterProxy extends \Relay\Cluster implements ResetInterface, LazyOb
     public function xdel($key, $ids): \Relay\Cluster|false|int
     {
         return $this->initializeLazyObject()->xdel(...\func_get_args());
+    }
+
+    public function xdelex($key, $ids, $mode = null): \Relay\Cluster|array|false
+    {
+        return $this->initializeLazyObject()->xdelex(...\func_get_args());
     }
 
     public function xgroup($operation, $key = null, $group = null, $id_or_consumer = null, $mkstream = false, $entries_read = -2): mixed
