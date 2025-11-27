ddf04121e7ebcfb9bee6ac59639a7f246f54afb0
Christian Flothmann <christian.flothmann@open.de>
2025-11-27 16:25:48 +0100

    ensure compatibility with Relay extension 0.20.0

--- Traits/RelayProxy.php
+++ Traits/RelayProxy.php
@@ -25,6 +25,7 @@ use Symfony\Component\Cache\Traits\Relay\PfcountTrait;
 use Symfony\Component\Cache\Traits\Relay\Relay11Trait;
 use Symfony\Component\Cache\Traits\Relay\Relay121Trait;
 use Symfony\Component\Cache\Traits\Relay\Relay12Trait;
+use Symfony\Component\Cache\Traits\Relay\Relay20Trait;
 use Symfony\Component\Cache\Traits\Relay\SwapdbTrait;
 use Symfony\Component\VarExporter\LazyObjectInterface;
 use Symfony\Component\VarExporter\LazyProxyTrait;
@@ -58,6 +59,7 @@ class RelayProxy extends \Relay\Relay implements ResetInterface, LazyObjectInter
     use Relay11Trait;
     use Relay12Trait;
     use Relay121Trait;
+    use Relay20Trait;
     use SwapdbTrait;
 
     private const LAZY_OBJECT_PROPERTY_SCOPES = [];
